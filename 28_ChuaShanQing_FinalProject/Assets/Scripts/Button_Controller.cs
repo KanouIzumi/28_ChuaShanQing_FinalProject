296c8d868ec4cdf6805ed08f94cd44c8ef0edd68
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-02-08 20:56:54 +0800

    Button done

--- 28_ChuaShanQing_FinalProject/Assets/Scripts/Button_Controller.cs
+++ 28_ChuaShanQing_FinalProject/Assets/Scripts/Button_Controller.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Button_Controller : MonoBehaviour
 {
@@ -14,5 +15,25 @@ public class Button_Controller : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Instruction()
+    {
+        SceneManager.LoadScene("InstructionsScene");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("CreditScene");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 }
